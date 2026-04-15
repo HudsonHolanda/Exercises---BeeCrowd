7f4fbca8c1c786a3891dbb730aa4132d780933f0
HudsonHolanda <hudsonyure4@gmail.com>
2026-04-14 23:11:04 -0300

    add exercises 1013 to 1018

---
+++ Exercises/1014.py
@@ -0,0 +1,6 @@
+Distancia_percorrida = float(input())
+Combustivel_gasto = float(input())
+
+Consumo_medio = Distancia_percorrida / Combustivel_gasto
+
+print(f"{Consumo_medio:.3f} km/l")
---
+++ Exercises/1017.py
@@ -0,0 +1,6 @@
+tempo = int(input())
+velocidade = int(input())
+
+combustivel = (tempo * velocidade) / 12
+
+print(f"{combustivel:.3f}")
